d737a0e0c432634c30fb06a07cb26333ecb8524f
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-27 16:54:37 -0400

    add router

--- issue.js
+++ issue.js
@@ -10,6 +10,12 @@ async function list(_, { status }) {
   return issues;
 }
 
+async function get(_, { id }) {
+  const db = getDb();
+  const issue = await db.collection('issues').findOne({ id });
+  return issue;
+}
+
 function validate(issue) {
   const errors = [];
   if (issue.title.length < 3) {
@@ -36,4 +42,4 @@ async function add(_, { issue }) {
   return savedIssue;
 }
 
-module.exports = { list, add };
+module.exports = { list, add, get };
