db32cd31f06298c3a68ea0b962ea4959319e768e
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-31 10:57:52 -0400

    add delete api

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -9,6 +9,7 @@
 /* eslint no-restricted-globals: "off" */
 
 db.issues.remove({});
+db.deleted_issues.remove({});
 
 const issuesDB = [
   {
@@ -66,3 +67,5 @@ db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
 db.issues.createIndex({ owner: 1 });
 db.issues.createIndex({ created: 1 });
+
+db.deleted_issues.createIndex({ id: 1 }, { unique: true });
