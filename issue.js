9c53596d0bcf2b1ca1d78dc72ce4d4950613827b
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-06-10 10:39:00 -0400

    add pagition api

--- issue.js
+++ issue.js
@@ -2,7 +2,11 @@ const { UserInputError } = require('apollo-server-express');
 
 const { getDb, getNextSequence } = require('./db.js');
 
-async function list(_, { status, effortMin, effortMax }) {
+const PAGE_SIZE = 10;
+
+async function list(_, {
+  status, effortMin, effortMax, page,
+}) {
   const db = getDb();
   const filter = {};
   if (status) filter.status = status;
@@ -11,8 +15,15 @@ async function list(_, { status, effortMin, effortMax }) {
     if (effortMin !== undefined) filter.effort.$gte = effortMin;
     if (effortMax !== undefined) filter.effort.$lte = effortMax;
   }
-  const issues = await db.collection('issues').find(filter).toArray();
-  return issues;
+  const cursor = db.collection('issues').find(filter)
+    .sort({ id: 1 })
+    .skip(PAGE_SIZE * (page - 1))
+    .limit(PAGE_SIZE);
+  const totalCount = await cursor.count(false);
+  const issues = cursor.toArray();
+  const pages = Math.ceil(totalCount / PAGE_SIZE);
+
+  return { issues, pages };
 }
 
 async function get(_, { id }) {
